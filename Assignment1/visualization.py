31993df8ba01e43d058c498be710ebf6fcb60d71
Koko <koko@Kokos-MacBook-Pro.local>
2019-02-07 19:49:38 -0500

    took out linear_model module

--- Assignment1/visualization.py
+++ Assignment1/visualization.py
@@ -52,27 +52,23 @@ for i in range(11,21):
     x_poly_20 = np.hstack((x_poly_20,x_raised_to_power))
 
 # create the erm models for polynomial degree of 1, 5, 10 and 20
-erm_1 = linear_model.LinearRegression()
-erm_5 = linear_model.LinearRegression()
-erm_10 = linear_model.LinearRegression()
-erm_20 = linear_model.LinearRegression()
 # Train the models using the training data 
-erm_1.fit(x_poly_1, y_train)
-erm_5.fit(x_poly_5, y_train)
-erm_10.fit(x_poly_10, y_train)
-erm_20.fit(x_poly_20, y_train)
+w_erm_1 =  np.linalg.solve(np.matmul(x_poly_1.T , x_poly_1), np.matmul(x_poly_1.T , y_train))
+w_erm_5 =  np.linalg.solve(np.matmul(x_poly_5.T , x_poly_5), np.matmul(x_poly_5.T , y_train))
+w_erm_10 = np.linalg.solve(np.matmul(x_poly_10.T , x_poly_10), np.matmul(x_poly_10.T , y_train))
+w_erm_20 = np.linalg.solve(np.matmul(x_poly_20.T , x_poly_20), np.matmul(x_poly_20.T , y_train))
 
 # plot the polynomials for erm models with the input data as dot plot
 # where darker the color of the polynomial line the higher the degree of the polynomial
 plt.plot(input_data, output_data, 'o', color='black')
 
-plt.plot(X_plot, erm_1.intercept_ + X_plot*erm_1.coef_[1],color='#faff00')
+plt.plot(X_plot, w_erm_1[0] + X_plot*w_erm_1[1],color='#faff00')
 
-plt.plot(X_plot, erm_5.intercept_ + pow(X_plot,1)*erm_5.coef_[1]  + pow(X_plot,2)*erm_5.coef_[2]  + pow(X_plot,3)*erm_5.coef_[3]  + pow(X_plot,4)*erm_5.coef_[4]  + pow(X_plot,5)*erm_5.coef_[5],color='#ffd000')
+plt.plot(X_plot, w_erm_5[0] + pow(X_plot,1)*w_erm_5[1]  + pow(X_plot,2)*w_erm_5[2]  + pow(X_plot,3)*w_erm_5[3]  + pow(X_plot,4)*w_erm_5[4]  + pow(X_plot,5)*w_erm_5[5],color='#ffd000')
 
-plt.plot(X_plot, erm_10.intercept_ + pow(X_plot,1)*erm_10.coef_[1]  + pow(X_plot,2)*erm_10.coef_[2]  + pow(X_plot,3)*erm_10.coef_[3]  + pow(X_plot,4)*erm_10.coef_[4]  + pow(X_plot,5)*erm_10.coef_[5] + pow(X_plot,6)*erm_10.coef_[6]  + pow(X_plot,7)*erm_10.coef_[7]  + pow(X_plot,8)*erm_10.coef_[8]  + pow(X_plot,9)*erm_10.coef_[9]  + pow(X_plot,10)*erm_10.coef_[10] ,color='#ff8800')
+plt.plot(X_plot, w_erm_10[0] + pow(X_plot,1)*w_erm_10[1]  + pow(X_plot,2)*w_erm_10[2]  + pow(X_plot,3)*w_erm_10[3]  + pow(X_plot,4)*w_erm_10[4]  + pow(X_plot,5)*w_erm_10[5] + pow(X_plot,6)*w_erm_10[6]  + pow(X_plot,7)*w_erm_10[7]  + pow(X_plot,8)*w_erm_10[8]  + pow(X_plot,9)*w_erm_10[9]  + pow(X_plot,10)*w_erm_10[10] ,color='#ff8800')
 
-plt.plot(X_plot, erm_20.intercept_ + pow(X_plot,1)*erm_20.coef_[1]  + pow(X_plot,2)*erm_20.coef_[2]  + pow(X_plot,3)*erm_20.coef_[3]  + pow(X_plot,4)*erm_20.coef_[4]  + pow(X_plot,5)*erm_20.coef_[5] + pow(X_plot,6)*erm_20.coef_[6]  + pow(X_plot,7)*erm_20.coef_[7]  + pow(X_plot,8)*erm_20.coef_[8]  + pow(X_plot,9)*erm_20.coef_[9]  + pow(X_plot,10)*erm_20.coef_[10]  + pow(X_plot,11)*erm_20.coef_[11]  + pow(X_plot,12)*erm_20.coef_[12]  + pow(X_plot,13)*erm_20.coef_[13]  + pow(X_plot,14)*erm_20.coef_[14]  + pow(X_plot,15)*erm_20.coef_[15] + pow(X_plot,16)*erm_20.coef_[16]  + pow(X_plot,17)*erm_20.coef_[17]  + pow(X_plot,18)*erm_20.coef_[18]  + pow(X_plot,19)*erm_20.coef_[19]  + pow(X_plot,20)*erm_20.coef_[20] ,color='#ff0000')
+plt.plot(X_plot, w_erm_20[0] + pow(X_plot,1)*w_erm_20[1]  + pow(X_plot,2)*w_erm_20[2]  + pow(X_plot,3)*w_erm_20[3]  + pow(X_plot,4)*w_erm_20[4]  + pow(X_plot,5)*w_erm_20[5] + pow(X_plot,6)*w_erm_20[6]  + pow(X_plot,7)*w_erm_20[7]  + pow(X_plot,8)*w_erm_20[8]  + pow(X_plot,9)*w_erm_20[9]  + pow(X_plot,10)*w_erm_20[10]  + pow(X_plot,11)*w_erm_20[11]  + pow(X_plot,12)*w_erm_20[12]  + pow(X_plot,13)*w_erm_20[13]  + pow(X_plot,14)*w_erm_20[14]  + pow(X_plot,15)*w_erm_20[15] + pow(X_plot,16)*w_erm_20[16]  + pow(X_plot,17)*w_erm_20[17]  + pow(X_plot,18)*w_erm_20[18]  + pow(X_plot,19)*w_erm_20[19]  + pow(X_plot,20)*w_erm_20[20] ,color='#ff0000')
 
 plt.title('ERM')
 plt.xlabel('x')
@@ -80,29 +76,26 @@ plt.ylabel('y')
 plt.show()
 
 
+alpha = 0.001
+
 # create the rlm models for polynomial degree of 1, 5, 10 and 20 with regularization (where lambda = 0.001)
-rlm_1 = linear_model.Ridge(alpha=0.001,normalize=False)
-rlm_5 = linear_model.Ridge(alpha=0.001,normalize=False)
-rlm_10 = linear_model.Ridge(alpha=0.001,normalize=False)
-rlm_20 = linear_model.Ridge(alpha=0.001,normalize=False)
-# Train the rlm models using the training data 
-rlm_1.fit(x_poly_1, y_train)
-rlm_5.fit(x_poly_5, y_train)
-rlm_10.fit(x_poly_10, y_train)
-rlm_20.fit(x_poly_20, y_train)
+w_rlm_1 =  np.linalg.solve( (np.matmul(x_poly_1.T , x_poly_1) + (alpha * np.identity(x_poly_1.shape[1])) ), np.matmul(x_poly_1.T , y_train))
+w_rlm_5 =  np.linalg.solve( (np.matmul(x_poly_5.T , x_poly_5) + (alpha * np.identity(x_poly_5.shape[1])) ), np.matmul(x_poly_5.T , y_train))
+w_rlm_10 = np.linalg.solve( (np.matmul(x_poly_10.T , x_poly_10) + (alpha * np.identity(x_poly_10.shape[1])) ), np.matmul(x_poly_10.T , y_train))
+w_rlm_20 = np.linalg.solve( (np.matmul(x_poly_20.T , x_poly_20) + (alpha * np.identity(x_poly_20.shape[1])) ), np.matmul(x_poly_20.T , y_train))
 
 
 # plot the polynomials for rlm models with the input data as dot plot
 # where darker the color of the polynomial line the higher the degree of the polynomial
 plt.plot(input_data, output_data, 'o', color='black')
 
-plt.plot(X_plot, rlm_1.intercept_ + X_plot*rlm_1.coef_[1],color='#faff00')
+plt.plot(X_plot, w_rlm_1[0] + X_plot*w_rlm_1[1],color='#faff00')
 
-plt.plot(X_plot, rlm_5.intercept_ + pow(X_plot,1)*rlm_5.coef_[1]  + pow(X_plot,2)*rlm_5.coef_[2]  + pow(X_plot,3)*rlm_5.coef_[3]  + pow(X_plot,4)*rlm_5.coef_[4]  + pow(X_plot,5)*rlm_5.coef_[5],color='#ffd000')
+plt.plot(X_plot, w_rlm_5[0] + pow(X_plot,1)*w_rlm_5[1]  + pow(X_plot,2)*w_rlm_5[2]  + pow(X_plot,3)*w_rlm_5[3]  + pow(X_plot,4)*w_rlm_5[4]  + pow(X_plot,5)*w_rlm_5[5],color='#ffd000')
 
-plt.plot(X_plot, rlm_10.intercept_ + pow(X_plot,1)*rlm_10.coef_[1]  + pow(X_plot,2)*rlm_10.coef_[2]  + pow(X_plot,3)*rlm_10.coef_[3]  + pow(X_plot,4)*rlm_10.coef_[4]  + pow(X_plot,5)*rlm_10.coef_[5] + pow(X_plot,6)*rlm_10.coef_[6]  + pow(X_plot,7)*rlm_10.coef_[7]  + pow(X_plot,8)*rlm_10.coef_[8]  + pow(X_plot,9)*rlm_10.coef_[9]  + pow(X_plot,10)*rlm_10.coef_[10] ,color='#ff8800')
+plt.plot(X_plot, w_rlm_10[0] + pow(X_plot,1)*w_rlm_10[1]  + pow(X_plot,2)*w_rlm_10[2]  + pow(X_plot,3)*w_rlm_10[3]  + pow(X_plot,4)*w_rlm_10[4]  + pow(X_plot,5)*w_rlm_10[5] + pow(X_plot,6)*w_rlm_10[6]  + pow(X_plot,7)*w_rlm_10[7]  + pow(X_plot,8)*w_rlm_10[8]  + pow(X_plot,9)*w_rlm_10[9]  + pow(X_plot,10)*w_rlm_10[10] ,color='#ff8800')
 
-plt.plot(X_plot, rlm_20.intercept_ + pow(X_plot,1)*rlm_20.coef_[1]  + pow(X_plot,2)*rlm_20.coef_[2]  + pow(X_plot,3)*rlm_20.coef_[3]  + pow(X_plot,4)*rlm_20.coef_[4]  + pow(X_plot,5)*rlm_20.coef_[5] + pow(X_plot,6)*rlm_20.coef_[6]  + pow(X_plot,7)*rlm_20.coef_[7]  + pow(X_plot,8)*rlm_20.coef_[8]  + pow(X_plot,9)*rlm_20.coef_[9]  + pow(X_plot,10)*rlm_20.coef_[10]  + pow(X_plot,11)*rlm_20.coef_[11]  + pow(X_plot,12)*rlm_20.coef_[12]  + pow(X_plot,13)*rlm_20.coef_[13]  + pow(X_plot,14)*rlm_20.coef_[14]  + pow(X_plot,15)*rlm_20.coef_[15] + pow(X_plot,16)*rlm_20.coef_[16]  + pow(X_plot,17)*rlm_20.coef_[17]  + pow(X_plot,18)*rlm_20.coef_[18]  + pow(X_plot,19)*rlm_20.coef_[19]  + pow(X_plot,20)*rlm_20.coef_[20] ,color='#ff0000')
+plt.plot(X_plot, w_rlm_20[0] + pow(X_plot,1)*w_rlm_20[1]  + pow(X_plot,2)*w_rlm_20[2]  + pow(X_plot,3)*w_rlm_20[3]  + pow(X_plot,4)*w_rlm_20[4]  + pow(X_plot,5)*w_rlm_20[5] + pow(X_plot,6)*w_rlm_20[6]  + pow(X_plot,7)*w_rlm_20[7]  + pow(X_plot,8)*w_rlm_20[8]  + pow(X_plot,9)*w_rlm_20[9]  + pow(X_plot,10)*w_rlm_20[10]  + pow(X_plot,11)*w_rlm_20[11]  + pow(X_plot,12)*w_rlm_20[12]  + pow(X_plot,13)*w_rlm_20[13]  + pow(X_plot,14)*w_rlm_20[14]  + pow(X_plot,15)*w_rlm_20[15] + pow(X_plot,16)*w_rlm_20[16]  + pow(X_plot,17)*w_rlm_20[17]  + pow(X_plot,18)*w_rlm_20[18]  + pow(X_plot,19)*w_rlm_20[19]  + pow(X_plot,20)*w_rlm_20[20] ,color='#ff0000')
 
 plt.title('RLM alpha=0.001')
 plt.xlabel('x')
